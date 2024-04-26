a69a1ef44903ad3a6b3de8adaeb4aa090e333dca
dlskrud815 <dlskrud815@gmail.com>
2024-04-26 17:28:32 +0900

    Exercise2

--- 240426_cpp_practice1.cpp
+++ 240426_cpp_practice1.cpp
@@ -1,37 +1,66 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
-// xyArr 초기화 안 함 오류남 추후 수정
+//입력자료형 판단 추후 수정
+// 
+//함수 선언
+int InputCheck();
 
 int main()
 {
-	int x, y;
+	/*
+	// 1 글자만 판단
+	string name = "nia*258";
 
-	cout << "** 2차원 동적 배열 **" << endl << "( x,y에 -1 입력 시 종료)" << endl;
+	for (int i = 0; i < name.length(); i++)
+	{
+		cout << "name:\t" << name[i] << endl;
+		cout << "isdigit:\t" << isdigit(name[i]) << endl;
+		cout << "isalhpa:\t" << isalpha(name[i]) << endl;
+
+		if (isdigit(name[i]) != 0) //숫자
+		{
+
+		}
+		if (isalpha(name[i]) == 2) //문자
+		{
+
+		}
+	}
+	*/
+
+
+	///*
+	string input_x, input_y;
+
+	cout << "** 2차원 동적 배열 **" << endl
+		<< "두 개의 자연수를 입력해주세요" << endl
+		<< "( x,y에 -1 입력 시 종료 )" << endl;
 
 	while (1)
 	{
 		cout << endl << "x를 입력하세요: ";
-		cin >> x;
+		cin >> input_x;
 		cout << "y를 입력하세요: ";
-		cin >> y;
+		cin >> input_y;
 
-		int type;
-		int** xyArr; //2차원 동적 배열 변수 xyArr 
-
-		if (x == -1 && y == -1) {
-			type = 0;
+		if (input_x == "-1" && input_y == "-1") {
+			cout << endl << "종료합니다." << endl;
+			return 0;
 		}
-		else if (x <= 0 || y <= 0)
+		else if (stoi(input_x) <= 0 || stoi(input_y) <= 0)
 		{
-			type = 1;
+			cout << endl << "x와 y 모두에 양수를 입력해주세요." << endl;
 		}
-		else
+		else if (stoi(input_x) > 0 && stoi(input_y) > 0)
 		{
-			type = 2;
+			int x = stoi(input_x);
+			int y = stoi(input_y);
 
 			//2차원 동적 배열 xyArr 선언
-			xyArr = new int* [x];
+			int** xyArr = new int* [x];
+			int num = 1;
 
 			for (int i = 0; i < x; i++)
 			{
@@ -39,8 +68,6 @@ int main()
 			}
 
 			//arr[0][0] ~ arr[x][y] 순서대로 1부터 x*y 저장
-			int num = 1;
-
 			for (int j = 0; j < x; j++)
 			{
 				for (int k = 0; k < y; k++)
@@ -49,22 +76,7 @@ int main()
 					num++;
 				}
 			}
-		}
 
-
-		switch (type)
-		{
-		case 0:
-			cout << endl << "종료합니다." << endl;
-
-			return 0;
-
-		case 1:
-			cout << endl << "x와 y 모두에 양수를 입력해주세요." << endl;
-
-			break;
-
-		case 2:
 			//arr[0][0] ~ arr[x][y] 출력
 			for (int j = 0; j < x; j++)
 			{
@@ -84,8 +96,20 @@ int main()
 				delete[] xyArr[i];
 			}
 			delete[] xyArr;
-
-			break;
+		}
+		else
+		{
+			cout << "자연수를 입력해주세요." << endl;
 		}
 	}
+	//*/
+
 }
+
+/*
+//함수 본문
+int InputCheck()
+{
+	
+}
+*/
---
+++ Exercise2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector> //vector 헤더파일 추가
+
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+
+	cout << "몇 개의 정수를 입력? ";
+	cin >> num1;
+
+	cout << num1 << "개의 정수를 입력하세요." << endl << endl;
+	
+	//1.정수 저장용 빈 벡터 생성 후 크기 할당
+	vector<int> iVec(num1);
+
+	for (int i = 0; i < num1; i++)
+	{
+		cout << i << "번 째 정수: ";
+		cin >> num2;
+
+		//2.n개의 정수를 입력 받아 벡터에 추가
+		iVec[i] = num2;
+	}
+
+	//3.벡터의 크기 출력
+	cout << endl << "벡터의 크기는..\t\t" << iVec.size();
+	
+	//4.벡터의 모든 원소 출력
+	cout << endl << "벡터의 모든 원소는..\t";
+	int max = 0;
+
+	for (int j = 0; j < num1; j++)
+	{
+		cout << iVec[j] << " ";
+
+		//가장 큰 값 계산
+		if (max <= iVec[j])
+		{
+			max = iVec[j];
+		}
+
+		iVec[j] = iVec[j] * 2;
+	}
+
+	//5.가장 큰 값을 찾아 출력
+	cout << endl << "가장 큰 값은..\t\t" << max << endl;
+	 
+	//6.벡터의 모든 원소를 두배로
+	cout << "모든 원소를 두배로..\t";
+	for (int k = 0; k < iVec.size(); k++)
+	{
+		cout << iVec[k] << " ";
+	}
+
+	//345 출력 후 7 물어보고 출력 8 물어보고 출력
+	//7.인덱스를 입력받아 해당 인덱스에 있는 원소 제거
+	int index1;
+	cout << endl << "원소를 제거할 인덱스 입력..\t";
+	cin >> index1;
+
+	iVec.erase(iVec.begin() + index1);
+
+	for (int k = 0; k < iVec.size(); k++)
+	{
+		cout << iVec[k] << " ";
+
+	}
+	//8.인덱스를 입력받아 해당 인덱스에 새로운 원소 삽입
+	int index2, newValue;
+	cout << endl << "원소를 삽입할 인덱스 입력..\t";
+	cin >> index2;
+	cout << "새로운 원소 값 입력..\t";
+	cin >> newValue;
+
+	iVec.insert(iVec.begin() + index2, newValue);
+
+	for (int k = 0; k < iVec.size(); k++)
+	{
+		cout << iVec[k] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
